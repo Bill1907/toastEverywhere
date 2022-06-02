1daaacd8fb7603ce73a1b1c8fc58ccd592a2b6f8
boseong94 <boseong.kim@catenoid.net>
2022-06-02 20:40:57 +0900

    ..

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log('Hello World');
+document.body.onload = init;
+
+function init() {
+  let toastList = [];
+  const app: HTMLElement | null = document.getElementById('app');
+  if (app) {
+    addH1Title(app);
+    const toastArea = createToastArea(app);
+    createToast(toastArea);
+    createToast(toastArea);
+    createToast(toastArea);
+    createToast(toastArea);
+    const toast = createToast(toastArea);
+    console.log(toast);
+  }
+}
+
+function addH1Title(app: HTMLElement): void {
+  const element: HTMLElement = document.createElement('h1');
+  element.innerText = 'Delicious Toast';
+  app?.appendChild(element);
+}
+
+function createToastArea(app: HTMLElement): HTMLElement {
+  const toastArea: HTMLElement = document.createElement('div');
+  toastArea.className = 'toast-area';
+  app.appendChild(toastArea);
+  return toastArea;
+}
+
+function createToast(parent: HTMLElement): HTMLElement {
+  const element: HTMLElement = document.createElement('div');
+  element.className = 'toast';
+  parent?.appendChild(element);
+  return element;
+}
+
+function successToastStyle(toast: HTMLElement): void {
+  toast.style.backgroundColor = 'blue'
+
+}
